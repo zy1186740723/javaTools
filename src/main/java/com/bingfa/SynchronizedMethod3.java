b65a6856f846fe2717480507025f2146a5f60237
zy1186740723 <1186740723@qq.com>
2019-04-30 16:23:43 +0800

    Revert "first commit"
    This reverts commit f4b05f88048a1a4166c869533dd03dc32da6558d.

--- src/main/java/com/bingfa/SynchronizedMethod3.java
+++
@@ -1,9 +0,0 @@
-package com.bingfa;
-
-/**对象锁中：方法锁
- * @Author: zhangyan
- * @Date: 2019/4/16 23:36
- * @Version 1.0
- */
-public class SynchronizedMethod3 {
-}
--- src/main/java/com/zhangyan/myTest/TestDemo2.java
+++
@@ -1,9 +0,0 @@
-package com.zhangyan.myTest;
-
-/**
- * @Author: zhangyan
- * @Date: 2019/4/9 10:32
- * @Version 1.0
- */
-public class TestDemo2 {
-}
